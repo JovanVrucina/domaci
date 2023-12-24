b19acb7e55cc1eff75aa7b6a29435389786102b9
Jovan Novokmet <novokmet.jovan14@gmail.com>
2023-12-25 00:34:38 +0100

    za 4

--- domaci.js
+++ domaci.js
@@ -6,6 +6,30 @@ function renderArticle(article){
     <td><button class='oduzmi'>-</button><span class='brojac'>${article['komada']}</span><button class='dodaj'>+</button></td>
     <td class='UCena'>${article['cena']*article['komada']}</td>
     `)
+
+    $podaci.find('.oduzmi').on('click', function() {
+        if(parseInt($(this).parent().find('.brojac').text())>0){
+        article['komada']--;
+    
+        $(this).parent().find('.brojac').text(article['komada']);
+    
+        $(this).parent().parent().find('.UCena').text(article['cena']*article['komada']);
+        
+        renderSuma('.cenaSvega')
+        }
+    
+        });
+        $podaci.find('.dodaj').on('click', function() {
+
+            article['komada']++;
+    
+            $(this).parent().find('.brojac').text(article['komada']);
+    
+            $(this).parent().parent().find('.UCena').text(article['cena']*article['komada']);
+
+            renderSuma('.cenaSvega')
+        });
+    
     $stavka.append($podaci)
     return $stavka
 
@@ -13,16 +37,15 @@ function renderArticle(article){
 
 function renderArticleList(target, data){
     data.map(s => $(target).append(renderArticle(s)))
-    $(target).append(renderSuma())
+    $(target).append(renderSuma('.cenaSvega'))
 }
 
-function renderSuma(){
+function renderSuma(target){
     var suma = 0;
     $(".UCena").each(function(){
         suma += parseInt($(this).text());
     })
 
-    $sveukupno = $(`<p>${suma}</p>`);
-
-    return $sveukupno
+    $(target).text(suma)
 }
+
