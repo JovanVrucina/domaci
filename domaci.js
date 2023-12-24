2e6b1bbcb0414f9b1055d2e91e85f58075a04510
Jovan Novokmet <novokmet.jovan14@gmail.com>
2023-12-24 23:44:41 +0100

    za 3

--- domaci.js
+++ domaci.js
@@ -4,7 +4,7 @@ function renderArticle(article){
     <td>${article['naziv']}</td>
     <td>${article['cena']}</td>
     <td><button class='oduzmi'>-</button><span class='brojac'>${article['komada']}</span><button class='dodaj'>+</button></td>
-    <td>${article['cena']*article['komada']}</td>
+    <td class='UCena'>${article['cena']*article['komada']}</td>
     `)
     $stavka.append($podaci)
     return $stavka
@@ -13,4 +13,16 @@ function renderArticle(article){
 
 function renderArticleList(target, data){
     data.map(s => $(target).append(renderArticle(s)))
+    $(target).append(renderSuma())
+}
+
+function renderSuma(){
+    var suma = 0;
+    $(".UCena").each(function(){
+        suma += parseInt($(this).text());
+    })
+
+    $sveukupno = $(`<p>${suma}</p>`);
+
+    return $sveukupno
 }
