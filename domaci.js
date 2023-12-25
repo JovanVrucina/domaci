fa3369dd897abff1dfdbc122d2ff9c35ffaefd2c
Jovan Novokmet <novokmet.jovan14@gmail.com>
2023-12-25 06:43:40 +0100

    za 5

--- domaci.js
+++ domaci.js
@@ -49,3 +49,44 @@ function renderSuma(target){
     $(target).text(suma)
 }
 
+function dodajElement(input){
+   var nazivNovog = $(input).find('#nazivInput').val()
+   var cenaNovog = $(input).find('#cenaInput').val()
+   var kolicinaNovog = $(input).find('#kolicinaInput').val()
+
+   $tabela = $(input).parent().find('#tabela')
+   $stavka = $(`<tr></tr>`)
+   $podaci = $(`
+   <td>${nazivNovog}</td>
+   <td>${cenaNovog}</td>
+   <td><button class='oduzmi'>-</button><span class='brojac'>${kolicinaNovog}</span><button class='dodaj'>+</button></td>
+   <td class='UCena'>${cenaNovog*kolicinaNovog}</td>
+   `)
+
+   $podaci.find('.oduzmi').on('click', function() {
+    if(parseInt($(this).parent().find('.brojac').text())>0){
+    kolicinaNovog--;
+
+    $(this).parent().find('.brojac').text(kolicinaNovog);
+
+    $(this).parent().parent().find('.UCena').text(kolicinaNovog*cenaNovog);
+    
+    renderSuma('.cenaSvega')
+    }
+
+    });
+    $podaci.find('.dodaj').on('click', function() {
+
+        kolicinaNovog++;
+
+        $(this).parent().find('.brojac').text(kolicinaNovog);
+
+        $(this).parent().parent().find('.UCena').text(cenaNovog*kolicinaNovog);
+
+        renderSuma('.cenaSvega')
+    });
+
+   $stavka.append($podaci)
+   $tabela.append($stavka)
+   renderSuma('.cenaSvega')
+}
